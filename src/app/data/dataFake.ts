8900c55c021288b3e5ae50ddb16d84b6254d3dfe
Laiz <laizsc.89@gmail.com>
2024-01-29 10:23:50 -0300

    preparando a aplicação para utilizar dados dinâmicos

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,41 @@ export const dataFake = [
     "photo": "bbbb",
     "title": "saiu o novo filme",
     "description": "olá"
+  },
+  {
+    "id": 3,
+    "photo": "ccc",
+    "title": "saiu o novo filme",
+    "description": "olá"
+  },
+  {
+    "id": 4,
+    "photo": "dddd",
+    "title": "saiu o novo filme",
+    "description": "olá"
+  },
+  {
+    "id": 5,
+    "photo": "eeee",
+    "title": "saiu o novo filme",
+    "description": "olá"
+  },
+  {
+    "id": 6,
+    "photo": "ffff",
+    "title": "saiu o novo filme",
+    "description": "olá"
+  },
+  {
+    "id": 7,
+    "photo": "gggg",
+    "title": "saiu o novo filme",
+    "description": "olá"
+  },
+  {
+    "id": 8,
+    "photo": "hhhh",
+    "title": "saiu o novo filme",
+    "description": "olá"
   }
 ];
