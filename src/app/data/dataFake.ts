51968aec67d4569c6de95efd4a6f9f100884f94e
Laiz <laizsc.89@gmail.com>
2024-01-30 10:24:10 -0300

    deixa a content page dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,49 +1,49 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaa",
+    "id": "1",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 2,
-    "photo": "bbbb",
+    "id": "2",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 3,
-    "photo": "ccc",
+    "id": "3",
+    "photo": "chttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 4,
-    "photo": "dddd",
+    "id": "4",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 5,
-    "photo": "eeee",
+    "id": "5",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 6,
-    "photo": "ffff",
+    "id": "6",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 7,
-    "photo": "gggg",
+    "id": "7",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   },
   {
-    "id": 8,
-    "photo": "hhhh",
+    "id": "8",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU",
     "title": "saiu o novo filme",
     "description": "olá"
   }
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -8,9 +9,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit {
 
-  photoCover: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFbEpKqS1wETSZ-pOjjrv3le98VK6md09Z3g&usqp=CAU';
-  contentTitle: string = 'MINHA NOTÍCIA';
-  contentDescription: string = 'Olá Mundo!';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  private id:string|null = '';
 
   constructor(
     private route:ActivatedRoute
@@ -18,8 +20,19 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = (value.get("id"))
     )
+
+    this.setValuesToComponent(this.id)
+
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photo;
+
   }
 
 }
